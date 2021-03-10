286869ac4f70e0ddbfa58cf4f08d59920a4d6327
Michael Pasaye <mpasaye@ucsc.edu>
2021-03-10 11:49:21 -0800

    Made sure that the message_event is updated to reflect the payload message type

--- Lab_9.X/Message.c
+++ Lab_9.X/Message.c
@@ -15,7 +15,7 @@
 #include "Message.h"
 
 // Define Block
-#define CHECKSUMSIZE 2 // Accounts for both characters
+#define PAYLOADTYPESIZE 4 // Size of different payload types accounting for null terminator
 
 // Function Block
 
@@ -33,17 +33,53 @@ uint8_t Message_CalculateChecksum(const char* payload)
 
 int Message_ParseMessage(const char* payload, const char* checksum_string, BB_Event * message_event)
 {
-    char outCheckSumStr[CHECKSUMSIZE];
+    Message message;
+    int i;
+    char calcCheckSum[MESSAGE_CHECKSUM_LEN], payloadType[PAYLOADTYPESIZE];
+    sprintf(calcCheckSum, "%x", Message_CalculateChecksum(payload));
     
-    if (strlen(checksum_string) > CHECKSUMSIZE) {   // Makes sure that the checksum string is not greater than 2 characters
+    // Makes sure that the checksum string is exactly 2 characters
+    if (strlen(checksum_string) != MESSAGE_CHECKSUM_LEN) {
+        printf("INVALID CHECKSUM SIZE\n");
         return STANDARD_ERROR;
     }
-    sprintf(outCheckSumStr, "%x", Message_CalculateChecksum(payload));  // Calculates the actual checksum, and converts to str for comparison with passed checksum
     
-    if (strcmp(checksum_string, outCheckSumStr)) {
-        printf("strings are not equal\n");
-    } else {
-        printf("Strsings are equal\n");
+    // Makes sure that the calculated checksum and the passed are the same
+    if (strcmp(calcCheckSum, checksum_string)) {
+        printf("INEQUIVALENT CHECKSUM\n");
+        return STANDARD_ERROR;
     }
+    // Determine what the payload type is
+    for (i = 0; i < PAYLOADTYPESIZE - 1; i++) {
+        payloadType[i] = *payload++;
+    }
+    payloadType[i] = '\0'; // Appends the null terminator to the string
+    
+    // This block determines what the message type is and changes the BB_Event values
+    if (!strcmp(payloadType, "CHA")) {
+        printf("messageID: CHA\n");
+        message_event -> type = BB_EVENT_CHA_RECEIVED;
+    } else if (!strcmp(payloadType, "ACC")) {
+        printf("messageID: ACC\n");
+        message_event -> type = BB_EVENT_ACC_RECEIVED;
+    } else if (!strcmp(payloadType, "REV")) {
+        printf("messageID: REV\n");
+        message_event -> type = BB_EVENT_REV_RECEIVED;
+    } else if (!strcmp(payloadType, "SHO")) {
+        printf("messageID: SHO\n");
+        message_event->type = BB_EVENT_SHO_RECEIVED;
+    } else if (!strcmp(payloadType, "RES")) {
+        printf("messageID: RES\n");
+        message_event -> type = BB_EVENT_RES_RECEIVED;
+    } else {
+        printf("INVALID MESSAGE TYPE\n");
+        message_event -> type = BB_EVENT_NO_EVENT;
+        return STANDARD_ERROR;
+    }
+    //printf("message type: %d\n", message_event -> type);
+    
+    // Need to see if the payload template is a valid template
+    
+    
     return SUCCESS;
 }

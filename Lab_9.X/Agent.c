1bf1c226e197c59457ed12d8fd2d602317f1db84
Michael Pasaye <mpasaye@ucsc.edu>
2021-03-13 17:42:50 -0800

    Setting up the functions to complete AgentRun()

--- Lab_9.X/Agent.c
+++ Lab_9.X/Agent.c
@@ -1,1 +1,48 @@
+/* Creator: Michael Pasaye
+ * Class: CSE13E Winter 2021
+ * File: Agent.c
+ * Lab: BattleBoats
+ */
 
+// Standard Libraries
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+// CSE13E Libraries
+#include "BOARD.h"
+
+// BattleBoat Libraries
+#include "Agent.h"
+
+// Define Block
+
+// TypeDef Block
+
+// Variable Block
+
+static AgentState agentState;
+static int turnCnt;
+
+// Function Block
+
+void AgentInit(void)
+{
+    agentState = AGENT_STATE_START; // Initializing the state machine to Start
+    turnCnt = 0;    // Initialize the turn counter to zero
+}
+
+/*Message AgentRun(BB_Event event)
+{
+    
+}*/
+
+AgentState AgentGetState(void)
+{
+    return agentState;
+}
+
+void AgentSetState(AgentState newState)
+{
+    agentState = newState;
+}

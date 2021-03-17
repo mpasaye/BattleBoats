e16cdbf0fa5918f4af56d36195cafcce226f0eee
Michael Pasaye <mpasaye@ucsc.edu>
2021-03-16 23:48:01 -0700

    Updated the agents

--- Lab_9.X/AgentTest.c
+++ Lab_9.X/AgentTest.c
@@ -9,95 +9,109 @@
 #include "Negotiation.h"
 #include "BattleBoats.h"
 #include "Message.h"
-int main() {
+
+int main()
+{
     BOARD_Init();
     printf("Welcome to NABARRAZ'S AgentTest.  Compiled on %s %s\n", __DATE__, __TIME__);
     /**************************************************************************/
     //TESTING AGENT INIT
-
-    AgentInit();
-    printf("Testing AgentInit:\n");
-    AgentState return_val = AgentGetState();
-    if (return_val == AGENT_STATE_START) {
-        printf("TEST 1 PASSED\n");
-    } else {
-        printf("TEST 1 FAILED\n");
+    {
+        AgentInit();
+        int testspassed = 0, totaltests = 2;
+        
+        printf("Testing AgentInit:\n");
+        AgentState return_val = AgentGetState();
+        if (return_val == AGENT_STATE_START) {
+            testspassed++;
+            printf("TEST 1 PASSED\n");
+        } else {
+            printf("TEST 1 FAILED\n");
+        }
+        AgentSetState(AGENT_STATE_CHALLENGING);
+        AgentInit();
+        return_val = AgentGetState();
+        if (return_val == AGENT_STATE_START) {
+            testspassed++;
+            printf("TEST 2 PASSED\n");
+        } else {
+            printf("TEST 2 FAILED\n");
+        }
+        printf("%d/%d TESTS PASSED\n", testspassed, totaltests);
     }
-    AgentSetState(AGENT_STATE_CHALLENGING);
-    AgentInit();
-    return_val = AgentGetState();
-    if (return_val == AGENT_STATE_START) {
-        printf("TEST 2 PASSED\n");
-    } else {
-        printf("TEST 2 FAILED\n");
-    }
-
 
     /*******************************************************************************/
     //TESTING AGENT GET STATE
-    printf("Testing AgentGetState:\n");
-    int testspassed = 0;
-    AgentState newState = AGENT_STATE_CHALLENGING;
-    AgentSetState(newState);
-    return_val = AgentGetState();
-    if (return_val == newState) {
-        testspassed++;
-        printf("TEST 1 PASSED\n");
-    } else {
-        printf("TEST 1 FAILED\n");
-    }
+    {
+        AgentInit();
+        printf("Testing AgentGetState:\n");
+        int testspassed = 0, totaltests = 3;
+        AgentState newState = AGENT_STATE_CHALLENGING;
+        AgentSetState(newState);
+        AgentState return_val = AgentGetState();
+        if (return_val == newState) {
+            testspassed++;
+            printf("TEST 1 PASSED\n");
+        } else {
+            printf("TEST 1 FAILED\n");
+        }
 
-    newState = AGENT_STATE_WAITING_TO_SEND;
-    AgentSetState(newState);
-    return_val = AgentGetState();
-    if (return_val == newState) {
-        testspassed++;
-        printf("TEST 2 PASSED\n");
-    } else {
-        printf("TEST 2 FAILED\n");
-    }
+        newState = AGENT_STATE_WAITING_TO_SEND;
+        AgentSetState(newState);
+        return_val = AgentGetState();
+        if (return_val == newState) {
+            testspassed++;
+            printf("TEST 2 PASSED\n");
+        } else {
+            printf("TEST 2 FAILED\n");
+        }
 
-    newState = BB_EVENT_ACC_RECEIVED;
-    AgentSetState(newState);
-    return_val = AgentGetState();
-    if (return_val == newState) {
-        testspassed++;
-        printf("TEST 3 PASSED\n");
-    } else {
-        printf("TEST 3 FAILED\n");
+        newState = BB_EVENT_ACC_RECEIVED;
+        AgentSetState(newState);
+        return_val = AgentGetState();
+        if (return_val == newState) {
+            testspassed++;
+            printf("TEST 3 PASSED\n");
+        } else {
+            printf("TEST 3 FAILED\n");
+        }
+        printf("%d/%d TESTS PASSED\n", testspassed, totaltests);
     }
     /**************************************************************************/
     //TESTING AGENTSETSTATE
-    printf("Testing AgentSetState:\n");
-    testspassed = 0;
-    newState = AGENT_STATE_ATTACKING;
-    AgentSetState(newState);
-    return_val = AgentGetState();
-    if (return_val == newState) {
-        testspassed++;
-        printf("TEST 1 PASSED\n");
-    } else {
-        printf("TEST 1 FAILED\n");
-    }
+    {
+        printf("Testing AgentSetState:\n");
+        int testspassed = 0, totaltests = 3;
+        AgentState newState = AGENT_STATE_ATTACKING;
+        AgentSetState(newState);
+        AgentState return_val = AgentGetState();
+        if (return_val == newState) {
+            testspassed++;
+            printf("TEST 1 PASSED\n");
+        } else {
+            printf("TEST 1 FAILED\n");
+        }
 
-    newState = AGENT_STATE_ACCEPTING;
-    AgentSetState(newState);
-    return_val = AgentGetState();
-    if (return_val == newState) {
-        testspassed++;
-        printf("TEST 2 PASSED\n");
-    } else {
-        printf("TEST 2 FAILED\n");
-    }
+        newState = AGENT_STATE_ACCEPTING;
+        AgentSetState(newState);
+        return_val = AgentGetState();
+        if (return_val == newState) {
+            testspassed++;
+            printf("TEST 2 PASSED\n");
+        } else {
+            printf("TEST 2 FAILED\n");
+        }
 
-    newState = AGENT_STATE_END_SCREEN;
-    AgentSetState(newState);
-    return_val = AgentGetState();
-    if (return_val == newState) {
-        testspassed++;
-        printf("TEST 3 PASSED\n");
-    } else {
-        printf("TEST 3 FAILED\n");
+        newState = AGENT_STATE_END_SCREEN;
+        AgentSetState(newState);
+        return_val = AgentGetState();
+        if (return_val == newState) {
+            testspassed++;
+            printf("TEST 3 PASSED\n");
+        } else {
+            printf("TEST 3 FAILED\n");
+        }
+        printf("%d/%d TESTS PASSED\n", testspassed, totaltests);
     }
     /**
      * AgentRun evolves the Agent state machine in response to an event.
@@ -114,16 +128,43 @@ int main() {
     //Message AgentRun(BB_Event event);
     {
         AgentInit();
+        int testspassed = 0, totaltests = 3;
         printf("Testing AgentRun:\n");
-        BB_Event event1 = {BB_EVENT_START_BUTTON,2};
-        printf("printing");
-        Message message1 = AgentRun(event1);
-        printf("print something\n");
-        if (message1.type == MESSAGE_ACC) {
+        BB_Event event = {BB_EVENT_START_BUTTON};
+        Message message = AgentRun(event);
+        
+        if (message.type == MESSAGE_CHA) {
+            testspassed++;
             printf("TEST 1 PASSED\n");
         } else {
             printf("TEST 1 FAILED\n");
         }
+        
+        AgentInit();
+        event.type = BB_EVENT_ACC_RECEIVED;
+        event.param0 = 3;
+        message = AgentRun(event);
+        //AgentSetState(AGENT_STATE_START);
+        //message = AgentRun(event);
+        if (message.type == MESSAGE_CHA) {
+            testspassed++;
+            printf("TEST 2 PASSED\n");
+        } else {
+            printf("TEST 2 FAILED\n");
+        }
+        
+        AgentInit();
+        event.type = BB_EVENT_CHA_RECEIVED;
+        event.param0 = 3;
+        message = AgentRun(event);
+        
+        if (message.type == MESSAGE_ACC) {
+            testspassed++;
+            printf("TEST 3 PASSED\n");
+        } else {
+            printf("TEST 3 FAILED\n");
+        }
+        printf("%d/%d TESTS PASSED\n", testspassed, totaltests);
     }
     return 0;
     BOARD_End();
